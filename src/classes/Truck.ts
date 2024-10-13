1e5c62876b7c036083b4d0d204a7b3ac9043b00a
CityWasTaken <smithfaheed@gmail.com>
2024-10-13 18:53:50 -0400

    Finished the requirements for Motorbike and Trucks

--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -6,6 +6,9 @@ import Wheel from './Wheel.js';
 import AbleToTow from '../interfaces/AbleToTow.js';
 
 // TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
+// TODO: Declare properties of the Truck class
+// TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
+// TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
 class Truck extends Vehicle implements AbleToTow {
   vin: string;
   color: string;
@@ -16,8 +19,24 @@ class Truck extends Vehicle implements AbleToTow {
   topSpeed: number;
   wheels: Wheel[];
   towingCapacity: number;
-  // TODO: Declare properties of the Truck class
-  constructor(vin: string, color: string, make: string, model: string, year: number, weight: number, topSpeed: number, wheels: Wheel[], towingCapacity: number) {
+
+  // TODO: Create a constructor that accepts the properties of the Truck class
+    // TODO: The constructor should call the constructor of the parent class, Vehicle
+    // TODO: The constructor should initialize the properties of the Truck class
+    // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+    started: boolean,
+    currentSpeed: number,
+    towingCapacity: number
+  ) {
 
     super();
     this.vin = vin;
@@ -31,17 +50,14 @@ class Truck extends Vehicle implements AbleToTow {
     this.towingCapacity = towingCapacity;
 
   }
-  // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
-  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
 
-  // TODO: Create a constructor that accepts the properties of the Truck class
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
-    // TODO: The constructor should initialize the properties of the Truck class
-    // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
 
   // TODO: Implement the tow method from the AbleToTow interface
+  // TODO: Get the make an model of the vehicle if it exists
+  // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
+  // TODO: If it is, log that the vehicle is being towed
+  // TODO: If it is not, log that the vehicle is too heavy to be towed
   tow(vehicle: Truck | Motorbike | Car): void {
-    // TODO: Get the make an model of the vehicle if it exists
     if (vehicle.make && vehicle.model) {
       if (vehicle.weight <= this.towingCapacity) 
         console.log('The vehicle is being towed');
@@ -49,15 +65,33 @@ class Truck extends Vehicle implements AbleToTow {
       console.log('The vehicle cannot be towed due to its weight');
       
     }
-    // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
-    // TODO: If it is, log that the vehicle is being towed
-    // TODO: If it is not, log that the vehicle is too heavy to be towed
   }
 
   // TODO: Override the printDetails method from the Vehicle class
     // TODO: The method should call the printDetails method of the parent class
     // TODO: The method should log the details of the Truck
     // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
+
+    override printDetails(): void {
+      super.printDetails();
+
+      console.log(`VIN: ${this.vin}`);
+      console.log(`Make: ${this.make}`);
+      console.log(`Model: ${this.model}`);
+      console.log(`Year: ${this.year}`);
+      console.log(`Weight: ${this.weight} lbs`);
+      console.log(`Top Speed: ${this.topSpeed} mph`);
+      console.log(`Color: ${this.color}`);
+      console.log(`Towing Capacity: ${this.towingCapacity}`);
+      console.log(`Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`
+      );
+      console.log(`Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`
+      );
+      console.log(`Wheel 3: ${this.wheels[2].getDiameter} inch with a ${this.wheels[2].getTireBrand} tire`
+      );
+      console.log(`Wheel 4: ${this.wheels[3].getDiameter} inch with a ${this.wheels[3].getTireBrand} tire`
+      );
+    }
 }
 
 
